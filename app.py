ec2f4f4f3c36f986b8345a3e2da8e530845f8017
Sami Jakonen <sami.jakonen@iki.fi>
2020-03-07 20:57:28 +0200

    Create a skeleton for the CV creator page

--- app.py
+++ app.py
@@ -7,3 +7,26 @@
 #
 # BRIEF:
 # Main backend for ansio
+
+
+# Import libraries
+from flask import Flask, render_template
+from flask import request
+import requests
+
+
+# Initiate the flask app
+app = Flask(__name__)
+
+
+
+# Root path
+@app.route('/')
+def index():
+    # Print index.html
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.run(port=8003)
